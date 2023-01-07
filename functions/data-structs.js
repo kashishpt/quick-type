47659f5f9e547b7c9717ced5d49df45b787be63b
kashishpt <82962026+kashishpt@users.noreply.github.com>
2023-01-06 19:38:34 -0500

    support for strings in values

--- functions/data-structs.js
+++ functions/data-structs.js
@@ -6,85 +6,82 @@ async function toMap(input_language) {
     const selection = utils.getSelection()
 
     if (selection !== undefined) {
-        const regexp = new RegExp(utils.settings()['keyValueRegex'])
-        let text = selection.text
+        const regexp = new RegExp(utils.settings()['keyValueRegex'], 'g')
+        const matches = [...selection.text.matchAll(regexp)]
         let pairs = []
-        let pair = []
-        while (text.length !== 0 && text.match(regexp)) {
-            const key = text.match(regexp)
+        for (let i= 0; i < matches.length; i+=2) {
+            if (i === matches.length - 1) {
+                vscode.window.showInformationMessage('Odd number of inputs for map, adding dummy value at end...')
+                pairs.push([matches[i][0], undefined])
+            } else {
+                pairs.push([matches[i][0], matches[i+1][0]])
+            }
             
-
         }
-        console.log(text.match(regexp))
-    }
 
-//         for (let line of lines) {
-//             pairs.push(line.trim().split(' '))
-//         }
-// 
-//         const language = input_language === undefined ? vscode.window.activeTextEditor.document.languageId : input_language
-//         let output = ""
-// 
-//         if (language === 'javascript') {
-//             const type = await vscode.window.showQuickPick(['JavaScript Object ({ ... })', 'Map Object'], {title:'Which type of map would you like?'})
-// 
-//             if (type === 'JavaScript Object ({ ... })') {
-//                 output += "let _ = {\n"
-// 
-//                 for (const pair of pairs) {
-//                     output += `\t${pair[0]}: ${pair[1]},\n`
-//                 }
-// 
-//                 output += "}"
-//             } else if (type === 'Map Object') {
-//                 output += 'const quicktypeMap = new Map()\n'
-//                 for (const pair of pairs) {
-//                     output += `quicktypeMap.set(${pair[0]}, ${pair[1]})\n`
-//                 }
-//             } else {
-//                 output = selection.text
-//             }
-//         } else if (language === 'python') {
-//             output += "_ = {\n"
-// 
-//             for (const pair of pairs) {
-//                 output += `\t${pair[0]}: ${pair[1]},\n`
-//             }
-// 
-//             output += "}"
-//         } else if (language === 'java') {
-//             output += "Map<K, V> quickTypeMap = new HashMap<>();\n"
-//             
-//             for (const pair of pairs) {
-//                 output += `quickTypeMap.put(${pair[0]}, ${pair[1]});\n`
-//             }
-//         } else if (language === 'cpp') {
-//             output += "std::map<K, V> quickTypeMap;\n"
-//             
-//             for (const pair of pairs) {
-//                 output += `quickTypeMap[${pair[0]}] = ${pair[1]};\n`
-//             }
-//         } else if (language === 'ruby') {
-//             output += '_ = {\n'
-// 
-//             const entries = pairs.map(pair => `\t${pair[0]} => ${pair[1]}`)
-//             output += entries.join(',\n') + '\n}'
-//         }
-// 
-// 
-//         if (output === '') {
-//             const feedback = errors.languageNotSupported(['JavaScript', 'Python', 'Java', 'C++', 'Ruby'])
-//             if (feedback !== undefined) {
-//                 toMap(feedback)
-//             }
-//         } else {
-//             selection.editor.edit(editBuilder => editBuilder.replace(selection.range, output))
-//         }
-// 
-// 
-//     } else {
-//         errors.noHighlightedSection()
-    // }
+        const language = input_language === undefined ? vscode.window.activeTextEditor.document.languageId : input_language
+        let output = ""
+
+        if (language === 'javascript') {
+            const type = await vscode.window.showQuickPick(['JavaScript Object ({ ... })', 'Map Object'], {title:'Which type of map would you like?'})
+
+            if (type === 'JavaScript Object ({ ... })') {
+                output += "let _ = {\n"
+
+                for (const pair of pairs) {
+                    output += `\t${pair[0]}: ${pair[1]},\n`
+                }
+
+                output += "}"
+            } else if (type === 'Map Object') {
+                output += 'const quicktypeMap = new Map()\n'
+                for (const pair of pairs) {
+                    output += `quicktypeMap.set(${pair[0]}, ${pair[1]})\n`
+                }
+            } else {
+                output = selection.text
+            }
+        } else if (language === 'python') {
+            output += "_ = {\n"
+
+            for (const pair of pairs) {
+                output += `\t${pair[0]}: ${pair[1]},\n`
+            }
+
+            output += "}"
+        } else if (language === 'java') {
+            output += "Map<K, V> quickTypeMap = new HashMap<>();\n"
+            
+            for (const pair of pairs) {
+                output += `quickTypeMap.put(${pair[0]}, ${pair[1]});\n`
+            }
+        } else if (language === 'cpp') {
+            output += "std::map<K, V> quickTypeMap;\n"
+            
+            for (const pair of pairs) {
+                output += `quickTypeMap[${pair[0]}] = ${pair[1]};\n`
+            }
+        } else if (language === 'ruby') {
+            output += '_ = {\n'
+
+            const entries = pairs.map(pair => `\t${pair[0]} => ${pair[1]}`)
+            output += entries.join(',\n') + '\n}'
+        }
+
+
+        if (output === '') {
+            const feedback = errors.languageNotSupported(['JavaScript', 'Python', 'Java', 'C++', 'Ruby'])
+            if (feedback !== undefined) {
+                toMap(feedback)
+            }
+        } else {
+            selection.editor.edit(editBuilder => editBuilder.replace(selection.range, output))
+        }
+
+
+    } else {
+        errors.noHighlightedSection()
+    }
 }
 
 async function toArray(input_language) {
